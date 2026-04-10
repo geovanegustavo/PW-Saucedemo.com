4ae3edeac293b9caf5c8dffee38030dded5f5850
Geovane Torres <geovanegustavo@gmail.com>
2026-04-10 18:41:07 -0300

    Atualização testes de login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -31,6 +31,74 @@ test.describe('Testes de Login', () => {
         // Verificar se o login falhou exibindo a mensagem de erro
         await expect(loginPage.errorMessage).toBeVisible();
 
+        await expect(loginPage.errorMessage).toHaveText(
+            'Epic sadface: Username and password do not match any user in this service'
+        );
+
+        await loginPage.closeErrorMessage();
+
+        await expect(loginPage.errorMessage).not.toBeVisible();
+
+    });
+
+    test('Falha com campos vazios', async ({ page }) => {
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigateToUrl();
+        await loginPage.login('', '');
+
+        // Verificar se o login falhou exibindo a mensagem de erro
+        await expect(loginPage.errorMessage).toBeVisible();
+
+        await expect(loginPage.errorMessage).toHaveText(
+            'Epic sadface: Username is required'
+        );
+
+        await loginPage.closeErrorMessage();
+
+        await expect(loginPage.errorMessage).not.toBeVisible();
+
+    });
+
+    test('Falha com login vazio', async ({ page }) => {
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigateToUrl();
+        await loginPage.login('', 'secret_sauce');
+
+        // Verificar se o login falhou exibindo a mensagem de erro
+        await expect(loginPage.errorMessage).toBeVisible();
+
+        await expect(loginPage.errorMessage).toHaveText(
+            'Epic sadface: Username is required'
+        );
+
+        await loginPage.closeErrorMessage();
+
+        await expect(loginPage.errorMessage).not.toBeVisible();
+
+    });
+
+    test('Falha com senha vazia', async ({ page }) => {
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigateToUrl();
+        await loginPage.login('standard_user', '');
+
+        // Verificar se o login falhou exibindo a mensagem de erro
+        await expect(loginPage.errorMessage).toBeVisible();
+
+        await expect(loginPage.errorMessage).toHaveText(
+            'Epic sadface: Password is required'
+        );
+
+        await loginPage.closeErrorMessage();
+
+        await expect(loginPage.errorMessage).not.toBeVisible();
+
     });
 
 });
